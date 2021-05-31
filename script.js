aabf9d75f91e67c10fa44f6ede39fd955e963179
Quinn Herden <herdenq@gmail.com>
2021-05-31 12:48:37 -0400

    add empty state for each option

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var strings = ["E (high)", "B", "G", "D", "A", "E (low)"];
-var notes = ["A", "B", "C", "D", "E", "F", "G"];
-var mod = [" ", "#", "b"];
-var fingers = ["1st", "2nd", "3rd", "4th"];
+var strings = ["?", "E lo ", "B", "G", "D", "A", "E hi"];
+var notes = ["?", "A", "B", "C", "D", "E", "F", "G"];
+var mod = ["?", " ", "#", "b"];
+var fingers = ["?", "1st", "2nd", "3rd", "4th"];
 function change() {
-    document.getElementById("string").innerHTML = strings[Math.floor(Math.random() * strings.length)];
+    document.getElementById("string").innerHTML = strings[Math.floor(Math.random() * (strings.length-1)) + 1];
     document.getElementById("note").innerHTML =
-        notes[Math.floor(Math.random() * notes.length)] + mod[Math.floor(Math.random() * mod.length)];
-    document.getElementById("finger").innerHTML = fingers[Math.floor(Math.random() * fingers.length)];
+        notes[Math.floor(Math.random() * (notes.length-1)) + 1] + mod[Math.floor(Math.random() * (mod.length-1)) + 1];
+    document.getElementById("finger").innerHTML = fingers[Math.floor(Math.random() * (fingers.length-1)) + 1];
+}
+
+function selectAll() {
+    document.getElementsByClassName("")
 }
 
 document.addEventListener('keyup', event => {
