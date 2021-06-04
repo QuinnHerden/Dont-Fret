aef9f71f0c5f354dd604d629958b478e1852dd5b
Quinn Herden <herdenq@gmail.com>
2021-06-04 13:43:25 -0400

    complete note dictionary, with exception of theoretical keys

--- script.js
+++ script.js
@@ -10,30 +10,244 @@ const notes = {
             [10, 2]
         ]
     },
-    "D": {}
-}
-
-
-
-
-const base_notes = ["F", "C", "G", "D", "A", "E", "B"];
-var base_copy = base_notes.slice();
-
-function key_change() {
-    base_copy = base_notes.slice(); // copies base_notes
-    var accidentals = document.querySelector('input[name="key"]:checked').value[0]; // grabs the # of accidentals in selected key
+    "G": {
+        "notes_in_key": ['G', 'A', 'B', 'C', 'D', 'E', 'F#'],
+        "vertical_fretboard_locations": [
+            [0, 3],
+            [3, 0],
+            [3, 5],
+            [5, 2],
+            [8, 4],
+            [10, 1],
+            [12, 3]
+        ]
+    },
+    "D": {
+        "notes_in_key": ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'],
+        "vertical_fretboard_locations": [
+            [0, 2],
+            [3, 4],
+            [5, 1],
+            [7, 3],
+            [10, 0],
+            [10, 5],
+            [12, 2]
+        ]
+    },
+    "A": {
+        "notes_in_key": ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
+        "vertical_fretboard_locations": [
+            [0, 1],
+            [2, 3],
+            [5, 0],
+            [5, 5],
+            [7, 2],
+            [10, 4],
+            [12, 1]
+        ]
+    },
+    "E": {
+        "notes_in_key": ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#'],
+        "vertical_fretboard_locations": [
+            [0, 0],
+            [0, 5],
+            [2, 2],
+            [5, 4],
+            [7, 1],
+            [9, 3],
+            [12, 0],
+            [12, 5]
+        ]
+    },
+    "B": {
+        "notes_in_key": ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#'],
+        "vertical_fretboard_locations": [
+            [0, 4],
+            [2, 1],
+            [4, 3],
+            [7, 0],
+            [7, 5],
+            [9, 2],
+            [12, 4]
+        ]
+    },
+    "Cb": {
+        "notes_in_key": ['Cb', 'Db', 'Eb', 'Fb', 'Gb', 'Ab', 'Bb'],
+        "vertical_fretboard_locations": [
+            [0, 4],
+            [2, 1],
+            [4, 3],
+            [7, 0],
+            [7, 5],
+            [9, 2],
+            [12, 4]
+        ]
+    },
+    "F#": {
+        "notes_in_key": ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'E#'],
+        "vertical_fretboard_locations": [
+            [2, 0],
+            [2, 5],
+            [4, 2],
+            [7, 4],
+            [9, 1],
+            [11, 3]
+        ]
+    },
+    "Gb": {
+        "notes_in_key": ['Gb', 'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'F'],
+        "vertical_fretboard_locations": [
+            [2, 0],
+            [2, 5],
+            [4, 2],
+            [7, 4],
+            [9, 1],
+        ]
+    },
+    "Db": {
+        "notes_in_key": ['Db', 'Eb', 'F', 'Gb', 'Ab', 'Bb', 'C'],
+        "vertical_fretboard_locations": [
+            [2, 4],
+            [4, 1],
+            [6, 3],
+            [9, 0],
+            [9, 5],
+            [11, 2]
+        ]
+    },
+    "C#": {
+        "notes_in_key": ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#'],
+        "vertical_fretboard_locations": [
+            [2, 4],
+            [4, 1],
+            [6, 3],
+            [9, 0],
+            [9, 5],
+            [11, 2]
+        ]
+    },
+    "Ab": {
+        "notes_in_key": ['Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G'],
+        "vertical_fretboard_locations": [
+            [1, 3],
+            [4, 0],
+            [4, 5],
+            [6, 2],
+            [9, 4],
+            [11, 1]
+        ]
+    },
+    "Eb": {
+        "notes_in_key": ['Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'D'],
+        "vertical_fretboard_locations": [
+            [1, 2],
+            [4, 4],
+            [6, 1],
+            [8, 3],
+            [11, 0],
+            [11, 5]
+        ]
+    },
+    "Bb": {
+        "notes_in_key": ['Bb', 'C', 'D', 'Eb', 'F', 'G', 'A'],
+        "vertical_fretboard_locations": [
+            [1, 1],
+            [3, 3],
+            [6, 0],
+            [6, 5],
+            [8, 2],
+            [11, 4]
+        ]
+    },
+    "F": {
+        "notes_in_key": ['F', 'G', 'A', 'Bb', 'C', 'D', 'E'],
+        "vertical_fretboard_locations": [
+            [1, 0],
+            [1, 5],
+            [3, 2],
+            [6, 4],
+            [8, 1],
+            [10, 3]
+        ]
+    },
     
-    for (var i = 0; i < accidentals; i++) { // adds appropriate sharps
-        base_copy[i] = (base_copy[i] + "#");
+    // double sharps / flats
+
+    "Fb": { // E
+        "notes_in_key": [],
+        "vertical_fretboard_locations": [
+            [0, 0],
+            [0, 5],
+            [2, 2],
+            [5, 4],
+            [7, 2],
+            [9, 3],
+            [12, 0],
+            [12, 5]
+        ]
+    },
+    "A#": { // Bb
+        "notes_in_key": [],
+        "vertical_fretboard_locations": [
+            [1, 1],
+            [3, 3],
+            [6, 0],
+            [6, 5],
+            [8, 2],
+            [11, 4]
+        ]
+    },
+    "B#": { // C
+        "notes_in_key": [],
+        "vertical_fretboard_locations": [
+            [1, 4],
+            [3, 1],
+            [5, 3],
+            [8, 0],
+            [8, 5],
+            [10, 2]
+        ]
+    },
+    "D#": { // Eb
+        "notes_in_key": [],
+        "vertical_fretboard_locations": [
+            [1, 2],
+            [4, 4],
+            [6, 1],
+            [8, 3],
+            [11, 0],
+            [11, 5]
+        ]
+    },
+    "E#": { // F
+        "notes_in_key": [],
+        "vertical_fretboard_locations": [
+            [1, 0],
+            [1, 5],
+            [3, 2],
+            [6, 4],
+            [8, 1],
+            [10, 3]
+        ]
+    },
+    "G#": { // Ab
+        "notes_in_key": [],
+        "vertical_fretboard_locations": [
+            [1, 3],
+            [4, 0],
+            [4, 5],
+            [6, 2],
+            [9, 4],
+            [11, 1]
+        ]
     }
 }
 
+
+
 function change() {
-    // document.getElementById("note").innerHTML = tones["C"]["notes_in_key"][0];
-    document.getElementById("note").innerHTML = notes["C"]["vertical_fretboard_locations"][0];
-
-
-    // document.getElementById("note").innerHTML = notes[Math.floor(Math.random() * notes.length)];
+    document.getElementById("note").innerHTML = notes["G"]["notes_in_key"][6];
+    // document.getElementById("note").innerHTML = notes["C"]["vertical_fretboard_locations"];
 }
 
 document.addEventListener('keyup', event => {
