38704fd4d39c3eb22c773af0eff612d8d9322227
Quinn Herden <herdenq@gmail.com>
2021-06-06 13:36:26 -0400

    automate layout of a string's tuning on basic html grid

--- script.js
+++ script.js
@@ -1,250 +1,65 @@
-// const notes = {
-//     "C": {
-//         "notes_in_key": ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
-//         "vertical_fretboard_locations": [
-//             [1, 4],
-//             [3, 1],
-//             [5, 3],
-//             [8, 0],
-//             [8, 5],
-//             [10, 2]
-//         ]
-//     },
-//     "G": {
-//         "notes_in_key": ['G', 'A', 'B', 'C', 'D', 'E', 'F#'],
-//         "vertical_fretboard_locations": [
-//             [0, 3],
-//             [3, 0],
-//             [3, 5],
-//             [5, 2],
-//             [8, 4],
-//             [10, 1],
-//             [12, 3]
-//         ]
-//     },
-//     "D": {
-//         "notes_in_key": ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'],
-//         "vertical_fretboard_locations": [
-//             [0, 2],
-//             [3, 4],
-//             [5, 1],
-//             [7, 3],
-//             [10, 0],
-//             [10, 5],
-//             [12, 2]
-//         ]
-//     },
-//     "A": {
-//         "notes_in_key": ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
-//         "vertical_fretboard_locations": [
-//             [0, 1],
-//             [2, 3],
-//             [5, 0],
-//             [5, 5],
-//             [7, 2],
-//             [10, 4],
-//             [12, 1]
-//         ]
-//     },
-//     "E": {
-//         "notes_in_key": ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#'],
-//         "vertical_fretboard_locations": [
-//             [0, 0],
-//             [0, 5],
-//             [2, 2],
-//             [5, 4],
-//             [7, 1],
-//             [9, 3],
-//             [12, 0],
-//             [12, 5]
-//         ]
-//     },
-//     "B": {
-//         "notes_in_key": ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#'],
-//         "vertical_fretboard_locations": [
-//             [0, 4],
-//             [2, 1],
-//             [4, 3],
-//             [7, 0],
-//             [7, 5],
-//             [9, 2],
-//             [12, 4]
-//         ]
-//     },
-//     "Cb": {
-//         "notes_in_key": ['Cb', 'Db', 'Eb', 'Fb', 'Gb', 'Ab', 'Bb'],
-//         "vertical_fretboard_locations": [
-//             [0, 4],
-//             [2, 1],
-//             [4, 3],
-//             [7, 0],
-//             [7, 5],
-//             [9, 2],
-//             [12, 4]
-//         ]
-//     },
-//     "F#": {
-//         "notes_in_key": ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'E#'],
-//         "vertical_fretboard_locations": [
-//             [2, 0],
-//             [2, 5],
-//             [4, 2],
-//             [7, 4],
-//             [9, 1],
-//             [11, 3]
-//         ]
-//     },
-//     "Gb": {
-//         "notes_in_key": ['Gb', 'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'F'],
-//         "vertical_fretboard_locations": [
-//             [2, 0],
-//             [2, 5],
-//             [4, 2],
-//             [7, 4],
-//             [9, 1],
-//         ]
-//     },
-//     "Db": {
-//         "notes_in_key": ['Db', 'Eb', 'F', 'Gb', 'Ab', 'Bb', 'C'],
-//         "vertical_fretboard_locations": [
-//             [2, 4],
-//             [4, 1],
-//             [6, 3],
-//             [9, 0],
-//             [9, 5],
-//             [11, 2]
-//         ]
-//     },
-//     "C#": {
-//         "notes_in_key": ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#'],
-//         "vertical_fretboard_locations": [
-//             [2, 4],
-//             [4, 1],
-//             [6, 3],
-//             [9, 0],
-//             [9, 5],
-//             [11, 2]
-//         ]
-//     },
-//     "Ab": {
-//         "notes_in_key": ['Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G'],
-//         "vertical_fretboard_locations": [
-//             [1, 3],
-//             [4, 0],
-//             [4, 5],
-//             [6, 2],
-//             [9, 4],
-//             [11, 1]
-//         ]
-//     },
-//     "Eb": {
-//         "notes_in_key": ['Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'D'],
-//         "vertical_fretboard_locations": [
-//             [1, 2],
-//             [4, 4],
-//             [6, 1],
-//             [8, 3],
-//             [11, 0],
-//             [11, 5]
-//         ]
-//     },
-//     "Bb": {
-//         "notes_in_key": ['Bb', 'C', 'D', 'Eb', 'F', 'G', 'A'],
-//         "vertical_fretboard_locations": [
-//             [1, 1],
-//             [3, 3],
-//             [6, 0],
-//             [6, 5],
-//             [8, 2],
-//             [11, 4]
-//         ]
-//     },
-//     "F": {
-//         "notes_in_key": ['F', 'G', 'A', 'Bb', 'C', 'D', 'E'],
-//         "vertical_fretboard_locations": [
-//             [1, 0],
-//             [1, 5],
-//             [3, 2],
-//             [6, 4],
-//             [8, 1],
-//             [10, 3]
-//         ]
-//     },
-    
-//     // double sharps / flats
+var order = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+var ordered_notes = [
+    ['A', 'G##', 'Bbb'],
+    ['A#', 'Bb', 'Cbb'],
+    ['B', 'A##', 'Cb'],
+    ['C', 'B#', 'Dbb'],
+    ['C#', 'B##', 'Db'],
+    ['D', 'C##', 'Ebb'],
+    ['D#', 'C##', 'Eb'],
+    ['E', 'D##', 'Fb'],
+    ['F', 'E#', 'Gbb'],
+    ['F#', 'E##', 'Gb'],
+    ['G', 'F##', 'Abb'],
+    ['G#', 'Ab', 'Bbb'],
+];
 
-//     "Fb": { // E
-//         "notes_in_key": [],
-//         "vertical_fretboard_locations": [
-//             [0, 0],
-//             [0, 5],
-//             [2, 2],
-//             [5, 4],
-//             [7, 2],
-//             [9, 3],
-//             [12, 0],
-//             [12, 5]
-//         ]
-//     },
-//     "A#": { // Bb
-//         "notes_in_key": [],
-//         "vertical_fretboard_locations": [
-//             [1, 1],
-//             [3, 3],
-//             [6, 0],
-//             [6, 5],
-//             [8, 2],
-//             [11, 4]
-//         ]
-//     },
-//     "B#": { // C
-//         "notes_in_key": [],
-//         "vertical_fretboard_locations": [
-//             [1, 4],
-//             [3, 1],
-//             [5, 3],
-//             [8, 0],
-//             [8, 5],
-//             [10, 2]
-//         ]
-//     },
-//     "D#": { // Eb
-//         "notes_in_key": [],
-//         "vertical_fretboard_locations": [
-//             [1, 2],
-//             [4, 4],
-//             [6, 1],
-//             [8, 3],
-//             [11, 0],
-//             [11, 5]
-//         ]
-//     },
-//     "E#": { // F
-//         "notes_in_key": [],
-//         "vertical_fretboard_locations": [
-//             [1, 0],
-//             [1, 5],
-//             [3, 2],
-//             [6, 4],
-//             [8, 1],
-//             [10, 3]
-//         ]
-//     },
-//     "G#": { // Ab
-//         "notes_in_key": [],
-//         "vertical_fretboard_locations": [
-//             [1, 3],
-//             [4, 0],
-//             [4, 5],
-//             [6, 2],
-//             [9, 4],
-//             [11, 1]
-//         ]
+var num_of_frets = 12;
+var num_of_strings = 6;
+
+function layout(string_num, string_tuning) {
+    // Setting fret_index to starting location in ordered_notes list, based on string tuning
+    for (var i = 0; i < num_of_frets - 1; i++) {
+        for (var j = 0; j < num_of_strings - 1; j++) {
+            if (string_tuning == ordered_notes[i][j]) {
+                var fret_index = [i, j];
+            }
+        }
+    }
+
+    // Updating all parts grid to have note names listed
+    for (i = 0; i <= 12; i++) {
+        if (fret_index[0] > num_of_frets - 1) {
+            fret_index[0] = 0;
+        }
+        var id = "" + i + "," + string_num
+        document.getElementById(id).innerHTML= ordered_notes[fret_index[0]];
+        
+        fret_index[0] ++;
+    }
+}
+
+// function layout(string_num, string_tuning) {
+//     var fret_index = order.indexOf(string_tuning);
+
+//     for (var i = 0; i <= 12; i++) {
+//         if (fret_index > num_of_frets - 1) {
+//             fret_index = 0;
+//         }
+//         var id = "" + i + "," + string_num
+//         document.getElementById(id).innerHTML= order[fret_index];
+        
+//         fret_index ++;
 //     }
 // }
 
-function layout() {
-    document.getElementById("0,0")= "C";
-}
-
-layout();
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+        layout(0, 'F##');
+        layout(1, 'A');
+        layout(2, 'D');
+        layout(3, 'G');
+        layout(4, 'B');
+        layout(5, 'E');
+    }
+  })
