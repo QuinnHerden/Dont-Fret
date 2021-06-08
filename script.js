e93d15cf81cb44800e266dc6ac99894f7a78f18a
Quinn Herden <herdenq@gmail.com>
2021-06-08 19:01:12 -0400

    make display class

--- script.js
+++ script.js
@@ -100,52 +100,60 @@ class Piano extends Instrument {
 
 
 
-function show_board(instrument, selected_notes) {
-    var div;
-    var id_string;
-
-    for (var i = 0; i < instrument.num_of_runs; i++) {
-        div = document.createElement('div');
-        div.id = 'run' + i;
-        div.className = 'run';
-        document.getElementById('runs').appendChild(div);
-
-        for (var j = 0; j < instrument.steps_in_run; j++) {
+class Display {
+    constructor() {
+        this.instrument = new Guitar(['E', 'B', 'G', 'D']);
+        this.display_notes = ['C'];
+    }
+    show_board(instrument, selected_notes) {
+        var div;
+        var id_string;
+    
+        for (var i = 0; i < 4; i++) {
             div = document.createElement('div');
-            div.id = '' + i + ',' + j;
-            div.className = 'step';
-            // div.innerHTML = '';
-            id_string = 'run' + i;
-            document.getElementById(id_string).appendChild(div);
+            div.id = 'run' + i;
+            div.className = 'run';
+            document.getElementById('runs').appendChild(div);
+    
+            for (var j = 0; j < this.instrument.steps_in_run; j++) {
+                div = document.createElement('div');
+                div.id = '' + i + ',' + j;
+                div.className = 'step';
+                // div.innerHTML = '';
+                id_string = 'run' + i;
+                document.getElementById(id_string).appendChild(div);
+            }
+        }
+        show_board_util(instrument, selected_notes);
+    }
+    show_board_util(instrument, selected_notes) {
+        var to_fill;
+        var id_string;
+    
+        for (var i = 0; i < selected_notes.length; i++) {
+            to_fill = instrument.note_indexes[selected_notes[i]];
+    
+            for (var j = 0; j < to_fill.length; j++) {
+                id_string = '' + to_fill[j][0] + ',' + to_fill[j][1];
+                document.getElementById(id_string).innerHTML = selected_notes[i];
+            }
         }
     }
-    show_board_util(instrument, selected_notes);
-}
-function show_board_util(instrument, selected_notes) {
-    var to_fill;
-    var id_string;
-
-    for (var i = 0; i < selected_notes.length; i++) {
-        to_fill = instrument.note_indexes[selected_notes[i]];
-
-        for (var j = 0; j < to_fill.length; j++) {
-            id_string = '' + to_fill[j][0] + ',' + to_fill[j][1];
-            document.getElementById(id_string).innerHTML = selected_notes[i];
-        }
-    }
 }
 
+disp = new Display();
+disp.show_board();
 
 
-document.addEventListener('keyup', event => {
-    if (event.code === 'Digit1') {
-        var normiePiano = new Piano(['C']);
-        show_board(normiePiano, ['C', 'D', 'E', 'F', 'G', 'A', 'B'])
-    }
-})
-document.addEventListener('keyup', event => {
-    if (event.code === 'Digit2') {
-        var normieGuitar = new Guitar(['E', 'B', 'G', 'D', 'A', 'E']);
-        show_board(normieGuitar, ['C', 'G', 'E'])
-    }
-})
+// document.addEventListener('keyup', event => {
+//     if (event.code === 'Digit1') {
+//         var normiePiano = new Piano(['C']);
+//         show_board(normiePiano, ['C', 'D', 'E', 'F', 'G', 'A', 'B'])
+//     }
+// })
+// document.addEventListener('keyup', event => {
+//     if (event.code === 'Digit2') {
+//         var normieGuitar = new Guitar(['E', 'B', 'G', 'D', 'A', 'E']);
+//         show_board(normieGuitar, ['C', 'G', 'E'])
+//     }
+// })
